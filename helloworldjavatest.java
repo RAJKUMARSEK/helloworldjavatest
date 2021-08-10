9469ba2d85cbbe82035ad12e8122a591aa87031e
RAJKUMARSEK <88264286+RAJKUMARSEK@users.noreply.github.com>
2021-08-10 17:05:55 +0530

    Update helloworldjavatest.java

--- helloworldjavatest.java
+++ helloworldjavatest.java
@@ -2,6 +2,6 @@ public class helloworldjavatest{
 
 	public static void main(String[] args){
 		
-		System.out.println("Hello World - 10th Aug 2021");
+		System.out.println("Hello World testing- 10th Aug 2021");
 	}
 }
